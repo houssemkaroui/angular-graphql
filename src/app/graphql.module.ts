f49df4fb96c114b3f32b0365b865a0075142971d
BenSlimenMohamedAli <dslimen.15@gmail.com>
2023-03-15 17:03:58 +0100

    add graphql proxy and configure graphql

--- src/app/graphql.module.ts
+++ src/app/graphql.module.ts
@@ -3,15 +3,11 @@ import {ApolloModule, APOLLO_OPTIONS} from 'apollo-angular';
 import {ApolloClientOptions, InMemoryCache} from '@apollo/client/core';
 import {HttpLink} from 'apollo-angular/http';
 
-const uri = 'https://graphql.api.apollographql.com/api/graphql'; // <-- add the URL of the GraphQL server here
+const uri = '/api'; // <-- add the URL of the GraphQL server here
 export function createApollo(httpLink: HttpLink): ApolloClientOptions<any> {
   return {
     link: httpLink.create({uri}),
     cache: new InMemoryCache(),
-    headers: {
-      'Access-Control-Allow-Origin': '*',
-      
-    },
   };
 }
 
